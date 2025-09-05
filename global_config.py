0b3247d61fb33594164920ef79c5fdac7bd27c22
Bohdan Boklah <b.boklag@gmail.com>
2025-09-05 22:56:26 +0300

    add classes name

--- global_config.py
+++ global_config.py
@@ -1,3 +1,8 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
+
+IMAGE_DIR_TRAIN = BASE_DIR / 'stage_2_train_images'
+IMAGE_DIR_TEST = BASE_DIR / 'stage_2_test_images'
+
+class_names = ["Lung Opacity", "Normal"]
